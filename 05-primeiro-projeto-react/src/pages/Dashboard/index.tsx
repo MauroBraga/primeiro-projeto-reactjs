d1eafe90101c7c27552c05f44bed69cd34b4c45c
MauroBraga <mrb0305@gmail.com>
2020-11-09 21:13:38 -0300

    Salvando no storage

--- 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, FormEvent} from 'react';
+import React, {useState, FormEvent, useEffect} from 'react';
 import {FiChevronRight} from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg';
@@ -17,7 +17,21 @@ interface Repository {
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState('')
     const [inputError, setInputError]  = useState('')
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>(()=>{
+        const storageRepositories = localStorage.getItem('@GitHubExplorer:repositories')
+        if(storageRepositories){
+            return JSON.parse(storageRepositories)
+
+        }
+        return [];
+    });
+
+
+    useEffect(()=>{
+        localStorage.setItem(
+            '@GitHubExplorer:repositories',
+            JSON.stringify(repositories));
+    },[repositories])
 
     async function handleAddRepository(event:FormEvent<HTMLFormElement>):Promise<void>{
         event.preventDefault()
