28910abbb920d69886e3b1641387c1ee3ab3b0b6
MauroBraga <mrb0305@gmail.com>
2020-11-09 21:41:49 -0300

    Navegando entre rotas

--- 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState, FormEvent, useEffect} from 'react';
 import {FiChevronRight} from 'react-icons/fi'
-
+import {Link} from 'react-router-dom'
 import logoImg from '../../assets/logo.svg';
 import {Title, Form, Repositories, Error} from './styles';
 import api from '../../services/api'
@@ -71,8 +71,8 @@ const Dashboard: React.FC = () => {
                 {repositories.map(repository => {
                     return(
 
-                        <a key={repository.full_name}
-                            href= "teste">
+                        <Link key={repository.full_name}
+                            to= {`/repository/${repository.full_name}`}>
                             <img src={repository.owner.avatar_url} alt={repository.owner.login}></img>
 
                             <div>
@@ -82,7 +82,7 @@ const Dashboard: React.FC = () => {
 
                             <FiChevronRight size={20}/>
 
-                        </a>
+                        </Link>
 
                     )
                 })}
